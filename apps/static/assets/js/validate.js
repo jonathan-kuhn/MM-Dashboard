097970d05cb0d0fbcc5f9f479cf9f44e4394cd5d
jonathan-kuhn <jonathan.simon.kuhn@proton.me>
2024-10-18 09:35:11 +0200

    added so far untested version validation ode

--- apps/static/assets/js/validate.js
+++ apps/static/assets/js/validate.js
@@ -1,28 +1,53 @@
-function validateName(){
-    let name = document.getElementById('name').value;
-    const pattern = /^[a-zA-Z0-9_.-]+$/;
-    if (!name || name === "" || name == null) {
-        showError('No name specified!', 'name');
-    } else if (!pattern.test(name)) {
-            showError('Invalid Name!', 'name');
-    } else {
-        resetError('name')
-    }
+function validateName() {
+  let name = document.getElementById("name").value;
+  const pattern = /^[a-zA-Z0-9_.-]+$/;
+  if (!name || name === "" || name == null) {
+    showError("No name specified!", "name");
+  } else if (!pattern.test(name)) {
+    showError("Invalid Name!", "name");
+  } else {
+    resetError("name");
+  }
 }
 
+async function validateVersion() {
+  try {
+    let version = document.getElementById("VERSION").value;
+    const response = await fetch(
+      "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json"
+    );
+    const data = await response.json();
 
-function showError(errorMessage, inputField){
-    let field = document.getElementById(inputField);
-    field.classList.add('error');
-    if (inputField == 'guessedCategory') {
-        inputField.selectedIndex = 0;
-    } else {
-        field.value = null;
-        field.placeholder = errorMessage;
-    }
+    // Check if version exists in the versions array
+    const versionExists = data.versions.some(
+      (version) => version.id === version
+    );
+  } catch {
+    showError(
+      "There was a problem with the version validation, please contact the system admin",
+      "VERSION"
+    );
+  }
+
+  if (!version) {
+    showError("Invalid version!", "VERSION");
+  } else {
+    resetError("VERSION");
+  }
+}
+
+function showError(errorMessage, inputField) {
+  let field = document.getElementById(inputField);
+  field.classList.add("error");
+  if (inputField == "guessedCategory") {
+    inputField.selectedIndex = 0;
+  } else {
+    field.value = null;
+    field.placeholder = errorMessage;
+  }
 }
 
 function resetError(id) {
-    let field = document.getElementById(id);
-    field.classList.remove('error');
+  let field = document.getElementById(id);
+  field.classList.remove("error");
 }
